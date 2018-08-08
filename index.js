5c74e827bd3e1e7e3fa6dafd57de12ef65267824
github username <github email address>
2018-08-07 21:07:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(sel) {
+  return document.querySelector(sel)
+}
 
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('.target')
+}
+
+function deepestChild() {
+  return document.getElementById('grand-node').querySelector('div div div div div')
+
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('ul.ranked-list')
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+  }
+}
