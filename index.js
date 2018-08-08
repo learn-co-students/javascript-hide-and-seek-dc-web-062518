96842a9de7b78434468fbdf28fff54ea7a87f937
Alvaro De La Torre <alvarodltp@gmail.com>
2018-08-08 11:27:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.getElementById("nested").querySelector("div.target");
+}
+
+function deepestChild() {
+  return document.querySelector("#grand-node div div div div");
+}
+
+function increaseRankBy(n) {
+  let x = document.querySelectorAll("ul.ranked-list");
+  for (var i = 0; i < x.length; i++) {
+    x[i].innerHTML = parseInt(x[i].innerHTML) + n;
+  }
+  return x;
+}
